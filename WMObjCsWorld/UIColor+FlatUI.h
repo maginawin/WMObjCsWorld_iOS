96cbb7cea72c5ff15400baadce997ac6ab4bc8c8
maginawin <maginawin@163.com>
2015-07-31 18:54:36 +0800

    添加了 UIColor+FlatUI 的颜色 Category

---
+++ WMObjCsWorld/UIColor+FlatUI.h
@@ -0,0 +1,133 @@
+//
+//  UIColor+Category.h
+//  WMObjCsWorld
+//
+//  Created by maginawin on 15/7/31.
+//  Copyright (c) 2015年 wendong wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIColor (FlatUI)
+
+/**
+ * @brief flat 0, 0
+ * @return 青绿 rgb(26, 188, 156)
+ */
++ (UIColor *)flatTurquoiseAlpha:(CGFloat)alpha;
+
+/**
+ * @brief flat 0, 1
+ * @return 翠绿 rgb(46, 204, 113)
+ */
++ (UIColor *)flatEmeraldAlpha:(CGFloat)alpha;
+
+/**
+ * @brief flat 0, 2
+ * @return 河蓝 rgb(52, 152, 219)
+ */
++ (UIColor *)flatPeterRiverAlpha:(CGFloat)alpha;
+
+/**
+ * @brief flat 0, 3
+ * @return 浅紫 rgb(155, 89, 182)
+ */
++ (UIColor *)flatAmethystAlpha:(CGFloat)alpha;
+
+/**
+ * @brief flat 0, 4
+ * @return 湿沥青 rgb(52, 73, 94)
+ */
++ (UIColor *)flatWetAsphaltAlpha:(CGFloat)alpha;
+
+/**
+ * @brief flat 1, 0
+ * @return 海绿 rgb(22, 160, 133)
+ */
++ (UIColor *)flatGreenSeaAlpha:(CGFloat)alpha;
+
+/**
+ * @brief flat 1, 1
+ * @return 深绿 rgb(39, 174, 96)
+ */
++ (UIColor *)flatNephritisAlpha:(CGFloat)alpha;
+
+/**
+ * @brief flat 1, 2
+ * @return 深蓝 rgb(41, 128, 185)
+ */
++ (UIColor *)flatBelizeHoleAlpha:(CGFloat)alpha;
+
+/**
+ * @brief flat 1, 3
+ * @return 深紫 rgb(142, 68, 173)
+ */
++ (UIColor *)flatWisteriaAlpha:(CGFloat)alpha;
+
+/**
+ * @brief flat 1, 4
+ * @return 午夜黑 rgb(44, 62, 80)
+ */
++ (UIColor *)flatMidnightBlueAlpha:(CGFloat)alpha;
+
+/**
+ * @brief flat 2, 0
+ * @return 太阳黄 rgb(241, 196, 15)
+ */
++ (UIColor *)flatSunFlowerAlpha:(CGFloat)alpha;
+
+/**
+ * @brief flat 2, 1
+ * @return 胡萝卜橙 rgb(230, 126, 34)
+ */
++ (UIColor *)flatCarrotAlpha:(CGFloat)alpha;
+
+/**
+ * @brief flat 2, 2
+ * @return 茜红 rgb(231, 76, 60)
+ */
++ (UIColor *)flatAlizarinAlpha:(CGFloat)alpha;
+
+/**
+ * @brief flat 2, 3
+ * @return 云白 rgb(236, 240, 241)
+ */
++ (UIColor *)flatCloudsAlpha:(CGFloat)alpha;
+
+/**
+ * @brief flat 2, 4
+ * @return 水泥灰 rgb(149, 165, 166)
+ */
++ (UIColor *)flatConcreteAlpha:(CGFloat)alpha;
+
+/**
+ * @brief flat 3, 0
+ * @return 脐橙 rgb(243, 156, 18)
+ */
++ (UIColor *)flatOrangeAlpha:(CGFloat)alpha;
+
+/**
+ * @brief flat 3, 1
+ * @return 南瓜橙 rgb(211, 84, 0)
+ */
++ (UIColor *)flatPumpkinAlpha:(CGFloat)alpha;
+
+/**
+ * @brief flat 3, 2
+ * @return 石榴红 rgb(192, 57, 43)
+ */
++ (UIColor *)flatPomegranateAlpha:(CGFloat)alpha;
+
+/**
+ * @brief flat 3, 3
+ * @return 银白 rgb(189, 195, 199)
+ */
++ (UIColor *)flatSliverAlpha:(CGFloat)alpha;
+
+/**
+ * @brief flat 3, 4
+ * @return 石棉灰 rgb(127, 140, 141)
+ */
++ (UIColor *)flatAsbestosAlpha:(CGFloat)alpha;
+
+@end
---
+++ WMObjCsWorld/UIColor+FlatUI.m
@@ -0,0 +1,93 @@
+//
+//  UIColor+Category.m
+//  WMObjCsWorld
+//
+//  Created by maginawin on 15/7/31.
+//  Copyright (c) 2015年 wendong wang. All rights reserved.
+//
+
+#import "UIColor+FlatUI.h"
+
+@implementation UIColor (FlatUI)
+
++ (UIColor *)flatTurquoiseAlpha:(CGFloat)alpha {
+    return [UIColor colorWithRed:26/255.0 green:188/255.f blue:156/255.f alpha:alpha];
+}
+
++ (UIColor *)flatEmeraldAlpha:(CGFloat)alpha {
+    return [UIColor colorWithRed:46/255.0 green:204/255.f blue:133/255.f alpha:alpha];
+}
+
++ (UIColor *)flatPeterRiverAlpha:(CGFloat)alpha {
+    return [UIColor colorWithRed:52/255.0 green:152/255.f blue:219/255.f alpha:alpha];
+}
+
++ (UIColor *)flatAmethystAlpha:(CGFloat)alpha {
+    return [UIColor colorWithRed:155/255.0 green:89/255.f blue:182/255.f alpha:alpha];
+}
+
++ (UIColor *)flatWetAsphaltAlpha:(CGFloat)alpha {
+    return [UIColor colorWithRed:52/255.0 green:73/255.f blue:94/255.f alpha:alpha];
+}
+
++ (UIColor *)flatGreenSeaAlpha:(CGFloat)alpha {
+    return [UIColor colorWithRed:22/255.0 green:160/255.f blue:133/255.f alpha:alpha];
+}
+
++ (UIColor *)flatNephritisAlpha:(CGFloat)alpha {
+    return [UIColor colorWithRed:39/255.0 green:174/255.f blue:96/255.f alpha:alpha];
+}
+
++ (UIColor *)flatBelizeHoleAlpha:(CGFloat)alpha {
+    return [UIColor colorWithRed:41/255.0 green:128/255.f blue:185/255.f alpha:alpha];
+}
+
++ (UIColor *)flatWisteriaAlpha:(CGFloat)alpha {
+    return [UIColor colorWithRed:142/255.0 green:68/255.f blue:173/255.f alpha:alpha];
+}
+
++ (UIColor *)flatMidnightBlueAlpha:(CGFloat)alpha {
+    return [UIColor colorWithRed:44/255.0 green:62/255.f blue:80/255.f alpha:alpha];
+}
+
++ (UIColor *)flatSunFlowerAlpha:(CGFloat)alpha {
+    return [UIColor colorWithRed:241/255.0 green:196/255.f blue:15/255.f alpha:alpha];
+}
+
++ (UIColor *)flatCarrotAlpha:(CGFloat)alpha {
+    return [UIColor colorWithRed:230/255.0 green:126/255.f blue:34/255.f alpha:alpha];
+}
+
++ (UIColor *)flatAlizarinAlpha:(CGFloat)alpha {
+    return [UIColor colorWithRed:231/255.0 green:76/255.f blue:60/255.f alpha:alpha];
+}
+
++ (UIColor *)flatCloudsAlpha:(CGFloat)alpha {
+    return [UIColor colorWithRed:236/255.0 green:240/255.f blue:241/255.f alpha:alpha];
+}
+
++ (UIColor *)flatConcreteAlpha:(CGFloat)alpha {
+    return [UIColor colorWithRed:149/255.0 green:165/255.f blue:166/255.f alpha:alpha];
+}
+
++ (UIColor *)flatOrangeAlpha:(CGFloat)alpha {
+    return [UIColor colorWithRed:243/255.0 green:156/255.f blue:18/255.f alpha:alpha];
+}
+
++ (UIColor *)flatPumpkinAlpha:(CGFloat)alpha {
+    return [UIColor colorWithRed:211/255.0 green:84/255.f blue:0/255.f alpha:alpha];
+}
+
++ (UIColor *)flatPomegranateAlpha:(CGFloat)alpha {
+    return [UIColor colorWithRed:192/255.0 green:57/255.f blue:43/255.f alpha:alpha];
+}
+
++ (UIColor *)flatSliverAlpha:(CGFloat)alpha {
+    return [UIColor colorWithRed:189/255.0 green:195/255.f blue:199/255.f alpha:alpha];
+}
+
++ (UIColor *)flatAsbestosAlpha:(CGFloat)alpha {
+    return [UIColor colorWithRed:127/255.0 green:140/255.f blue:141/255.f alpha:alpha];
+}
+
+@end
