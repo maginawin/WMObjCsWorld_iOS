82ff2d24fab9dba8badc7855178b87ffdf18c6a3
maginawin <maginawin@163.com>
2015-08-04 09:23:56 +0800

    flatcolor

--- WMObjCsWorld/UIColor+FlatUI.h
+++ WMObjCsWorld/UIColor+FlatUI.h
@@ -11,122 +11,102 @@
 @interface UIColor (FlatUI)
 
 /**
- * @brief flat 0, 0
- * @return 青绿 rgb(26, 188, 156)
+ * @brief flat 0, 0  青绿 rgb(26, 188, 156)
  */
 + (UIColor *)flatTurquoiseAlpha:(CGFloat)alpha;
 
 /**
- * @brief flat 0, 1
- * @return 翠绿 rgb(46, 204, 113)
+ * @brief flat 0, 1  翠绿 rgb(46, 204, 113)
  */
 + (UIColor *)flatEmeraldAlpha:(CGFloat)alpha;
 
 /**
- * @brief flat 0, 2
- * @return 河蓝 rgb(52, 152, 219)
+ * @brief flat 0, 2  河蓝 rgb(52, 152, 219)
  */
 + (UIColor *)flatPeterRiverAlpha:(CGFloat)alpha;
 
 /**
- * @brief flat 0, 3
- * @return 浅紫 rgb(155, 89, 182)
+ * @brief flat 0, 3  浅紫 rgb(155, 89, 182)
  */
 + (UIColor *)flatAmethystAlpha:(CGFloat)alpha;
 
 /**
- * @brief flat 0, 4
- * @return 湿沥青 rgb(52, 73, 94)
+ * @brief flat 0, 4  湿沥青 rgb(52, 73, 94)
  */
 + (UIColor *)flatWetAsphaltAlpha:(CGFloat)alpha;
 
 /**
- * @brief flat 1, 0
- * @return 海绿 rgb(22, 160, 133)
+ * @brief flat 1, 0  海绿 rgb(22, 160, 133)
  */
 + (UIColor *)flatGreenSeaAlpha:(CGFloat)alpha;
 
 /**
- * @brief flat 1, 1
- * @return 深绿 rgb(39, 174, 96)
+ * @brief flat 1, 1  深绿 rgb(39, 174, 96)
  */
 + (UIColor *)flatNephritisAlpha:(CGFloat)alpha;
 
 /**
- * @brief flat 1, 2
- * @return 深蓝 rgb(41, 128, 185)
+ * @brief flat 1, 2  深蓝 rgb(41, 128, 185)
  */
 + (UIColor *)flatBelizeHoleAlpha:(CGFloat)alpha;
 
 /**
- * @brief flat 1, 3
- * @return 深紫 rgb(142, 68, 173)
+ * @brief flat 1, 3  深紫 rgb(142, 68, 173)
  */
 + (UIColor *)flatWisteriaAlpha:(CGFloat)alpha;
 
 /**
- * @brief flat 1, 4
- * @return 午夜黑 rgb(44, 62, 80)
+ * @brief flat 1, 4  午夜黑 rgb(44, 62, 80)
  */
 + (UIColor *)flatMidnightBlueAlpha:(CGFloat)alpha;
 
 /**
- * @brief flat 2, 0
- * @return 太阳黄 rgb(241, 196, 15)
+ * @brief flat 2, 0  太阳黄 rgb(241, 196, 15)
  */
 + (UIColor *)flatSunFlowerAlpha:(CGFloat)alpha;
 
 /**
- * @brief flat 2, 1
- * @return 胡萝卜橙 rgb(230, 126, 34)
+ * @brief flat 2, 1  胡萝卜橙 rgb(230, 126, 34)
  */
 + (UIColor *)flatCarrotAlpha:(CGFloat)alpha;
 
 /**
- * @brief flat 2, 2
- * @return 茜红 rgb(231, 76, 60)
+ * @brief flat 2, 2  茜红 rgb(231, 76, 60)
  */
 + (UIColor *)flatAlizarinAlpha:(CGFloat)alpha;
 
 /**
- * @brief flat 2, 3
- * @return 云白 rgb(236, 240, 241)
+ * @brief flat 2, 3  云白 rgb(236, 240, 241)
  */
 + (UIColor *)flatCloudsAlpha:(CGFloat)alpha;
 
 /**
- * @brief flat 2, 4
- * @return 水泥灰 rgb(149, 165, 166)
+ * @brief flat 2, 4  水泥灰 rgb(149, 165, 166)
  */
 + (UIColor *)flatConcreteAlpha:(CGFloat)alpha;
 
 /**
- * @brief flat 3, 0
- * @return 脐橙 rgb(243, 156, 18)
+ * @brief flat 3, 0 脐橙 rgb(243, 156, 18)
  */
 + (UIColor *)flatOrangeAlpha:(CGFloat)alpha;
 
 /**
- * @brief flat 3, 1
- * @return 南瓜橙 rgb(211, 84, 0)
+ * @brief flat 3, 1  南瓜橙 rgb(211, 84, 0)
  */
 + (UIColor *)flatPumpkinAlpha:(CGFloat)alpha;
 
 /**
- * @brief flat 3, 2
- * @return 石榴红 rgb(192, 57, 43)
+ * @brief flat 3, 2  石榴红 rgb(192, 57, 43)
  */
 + (UIColor *)flatPomegranateAlpha:(CGFloat)alpha;
 
 /**
- * @brief flat 3, 3
- * @return 银白 rgb(189, 195, 199)
+ * @brief flat 3, 3  银白 rgb(189, 195, 199)
  */
 + (UIColor *)flatSliverAlpha:(CGFloat)alpha;
 
 /**
- * @brief flat 3, 4
- * @return 石棉灰 rgb(127, 140, 141)
+ * @brief flat 3, 4  石棉灰 rgb(127, 140, 141)
  */
 + (UIColor *)flatAsbestosAlpha:(CGFloat)alpha;
 
